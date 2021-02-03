9f5c18daea193d32b78c245235f9afec7a842c8c
bellhundred <good5229@naver.com>
2021-02-03 15:05:09 +0900

    feat(media) add media upload function

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -120,3 +122,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
