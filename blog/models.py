026341f0b8da72f77f44ca3939b511a26bb49050
bellhundred <good5229@naver.com>
2021-02-05 01:35:57 +0900

    feat(tag): add tag function and tag page

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,17 @@ from django.db import models
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -33,6 +44,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'{self.pk}-{self.title}'
