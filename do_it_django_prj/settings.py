8ea2d7f6adf56f6d1faa1082afdfd8d972870880
bellhundred <good5229@naver.com>
2021-02-06 01:38:30 +0900

    feat(crispy): add crispy_form

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
 
+    'crispy_forms',
+
     'blog',
     'single_pages',
 ]
@@ -128,3 +130,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
